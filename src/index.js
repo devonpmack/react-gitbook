fbe943cf633f9ed7c92fb3c3c58fb5dae162c515
devonpmack <devon.mack@shopify.com>
2019-04-01 11:11:27 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+let count = 50;
+let hasFire = false;
+
+const notificationNumber = (
+    <span className={"notificationNumber"}>
+        {
+            count > 99 ? '99+' : count
+        }
+    </span>
+);
+
+const notificationBadge = (
+    <div className = {
+        `bell ${hasFire ? 'has-fire' : ''}`
+    } >
+    {
+        count > 0 ? notificationNumber : null
+    }
+    </div>
+);
+
+ReactDOM.render(notificationBadge, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
